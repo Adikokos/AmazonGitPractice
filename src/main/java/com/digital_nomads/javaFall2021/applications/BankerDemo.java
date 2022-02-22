7f3f9fe9df75f9e9ca8ddee29661bd808af1f35a
Adilet <adilet.kenenbaev08@gnail.com>
2022-02-22 20:07:38 +0600

    deleted souts

--- src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
+++ src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
@@ -9,17 +9,16 @@ public class BankerDemo {
         DemirBank nursultanDemirBankAccount = new DemirBank(118000012341l, 123456789);
         DemirBank urmatDemirBankAccount = new DemirBank(118000003121l,987654321);
 
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
         nursultanDemirBankAccount.deposit(5000);
-        System.out.println("After deposit: " + nursultanDemirBankAccount.getBalance());
+
 
         transferFunds(nursultanDemirBankAccount,urmatDemirBankAccount,2000);
 
-        System.out.println("Urmat's  balance after transfer is : " + urmatDemirBankAccount.getBalance());
-        System.out.println("Nursultan's balnce after transfer is: " + nursultanDemirBankAccount.getBalance());
+
 
         transferFunds(urmatDemirBankAccount,nursultanDemirBankAccount,2101);
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
 
     }
 }
